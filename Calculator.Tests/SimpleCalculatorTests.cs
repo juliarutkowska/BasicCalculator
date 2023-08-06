806f263f9b5c40e1dc595979df46344a82716a32
juliarutkowska <julia.rutkowska189@gmail.com>
2023-08-06 14:51:16 +0200

    Using AutoData and AutoFixed in tests

--- Calculator.Tests/SimpleCalculatorTests.cs
+++ Calculator.Tests/SimpleCalculatorTests.cs
@@ -1,35 +1,41 @@
 using Shouldly;
+using AutoFixture.Xunit2;
 namespace Calculator.Tests;
 
 public class SimpleCalculatorTests
 {
     CalculatorOperations calculator = new CalculatorOperations();
 
-    [Fact]
-    public void Sum_of_two_numbers()
+    [Theory]
+    [AutoData]
+    public void Sum_of_two_numbers(int a, int b)
     {
-        var result = calculator.PerformCalculation("+", 5, 7);
-        result.ShouldBe(12);
+        var result = calculator.PerformCalculation("+", a, b);
+        result.ShouldBe(a + b);
     }
 
-    [Fact]
-    public void Difference_of_two_numbers()
+    [Theory]
+    [AutoData]
+    public void Difference_of_two_numbers(int a, int b)
     {
-        var result = calculator.PerformCalculation("-", 10, 3);
-        result.ShouldBe(7);
+        var result = calculator.PerformCalculation("-", a, b);
+        result.ShouldBe(a - b);
+    }
+    
+    [Theory]
+    [AutoData]
+    public void Product_of_two_numbers(int a, int b)
+    {
+        var result = calculator.PerformCalculation("*", a, b);
+        result.ShouldBe(a * b);
     }
 
-    [Fact]
-    public void Product_of_two_numbers()
+    [Theory]
+    [AutoData]
+    public void Quotient_of_two_numbers(int a, int b)
     {
-        var result = calculator.PerformCalculation("*", 5, 5);
-        result.ShouldBe(25);
-    }
-
-    [Fact]
-    public void Quotient_of_two_numbers()
-    {
-        var result = calculator.PerformCalculation("/", 30, 3);
-        result.ShouldBe(10);
+        if (b == 0) return;
+        var result = calculator.PerformCalculation("/", a, b);
+        result.ShouldBe(a / b);
     }
 }
